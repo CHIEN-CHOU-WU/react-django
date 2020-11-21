31d4a7a600bb1c8719f13a15e07cde5ad8189f51
CHIEN-CHOU-WU <johnson12440@gmail.com>
2020-11-20 21:28:56 -0600

    Changing Title tag for SEO

--- frontend/src/components/base/home.js
+++ frontend/src/components/base/home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="container">
       <h1>高雄 Zoe 手工香皂</h1>
-      
+
       <iframe
         src="http://www2.nkfust.edu.tw/~u0551103/buttons/6.gif"
         width="480"
